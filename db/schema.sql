70e0d04646c0b862e241a43845bda24b15a4dd03
louis.sussan@gmail.com <louis.sussan@gmail.com>
2017-08-13 09:20:39 -0700

    update css

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
 	`burger_name` varchar(255) NOT NULL,
 	`devoured` BOOLEAN DEFAULT FALSE,
 	`date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
